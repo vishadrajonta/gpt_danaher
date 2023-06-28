b715dab75ee7489c13e683c244be09fa4ffc7a4b
Vishad Raj Onta <vishadrajonta@gmail.com>
2023-06-28 17:12:35 -0500

    getting the hang of it kinda

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print("This is where the magic starts")
 print("Testing out this bs")
 print("Testing if this is just way, way easier on terminal")
+print("Okay, testing this one last time. Is it save, add, commit, and push?")
